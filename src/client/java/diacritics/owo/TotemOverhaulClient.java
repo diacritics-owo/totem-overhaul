5c0cbbbadd57144cefb82af80c924d41ba288924
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-08-21 09:11:59 +0530

    initial commit

---
+++ src/client/java/diacritics/owo/TotemOverhaulClient.java
@@ -0,0 +1,8 @@
+package diacritics.owo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TotemOverhaulClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
---
+++ src/main/java/diacritics/owo/TotemOverhaul.java
@@ -0,0 +1,15 @@
+package diacritics.owo;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TotemOverhaul implements ModInitializer {
+	public static final String MOD_ID = "totem-overhaul";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("hello from totem overhaul!");
+	}
+}
