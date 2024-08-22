e33402ea50e79f2b2a98772aad3e2394955a5dfd
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-08-22 18:19:02 +0530

    add cleric trade

--- src/main/java/diacritics/owo/TotemOverhaul.java
+++ src/main/java/diacritics/owo/TotemOverhaul.java
@@ -1,7 +1,16 @@
 package diacritics.owo;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import diacritics.owo.component.TotemOverhaulDataComponentTypes;
@@ -24,6 +33,16 @@ public class TotemOverhaul implements ModInitializer {
 		TotemOverhaulDataComponentTypes.initialize();
 		TotemOverhaulRecipeSerializers.initialize();
 		JewelEffects.initialize();
+
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 4, factory -> {
+			factory.add(new TradeOffers.Factory() {
+				@Override
+				public TradeOffer create(Entity entity, Random random) {
+					return new TradeOffer(new TradedItem(Items.EMERALD, random.nextBetween(8, 11)),
+							new ItemStack(Items.TOTEM_OF_UNDYING, 1), random.nextBetween(2, 5), 15, 1);
+				}
+			});
+		});
 	}
 
 	public static Identifier identifier(String path) {
